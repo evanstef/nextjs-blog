dbf45cc7c71989e0e5af331618b294c2df0ff28c
firewalker06 <didik.wicaksono@gmail.com>
2021-01-14 17:40:50 +0700

    Update text

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello, my name is Didik</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          This is the final leg of NextJS tutorial{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
